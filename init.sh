c6841dd86ef40dff11c213e6e08bc6d1530800c8
mohsin-riad <morteen1997@gmail.com>
2024-05-30 14:06:40 +0600

    code update

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 echo "Init Stable Diffusion Environment"
 sudo cog run python /src/init_env.py
 echo "Install Requirements"
-sudo cog run pip install --no-cache-dir -r requirements.txt "-e ."
+sudo cog run pip install --no-cache-dir -r requirements.txt
 echo "Download Models"
 command2
